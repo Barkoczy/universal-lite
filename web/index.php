805c7b68a804eb4aa859596c0cb0cc3442acc994
Henrich Barkoczy <henrich.barkoczy@tutanota.com>
2023-06-02 22:09:57 +0200

    Updated index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,15 @@
 <?php
+/**
+* Copyright 2023
+* 
+* @package    Universal Lite
+* @version		0.0.1
+*	@access			public
+* @author     Henrich Barkoczy <me@barkoczy.social>
+* @see 	      https://barkoczy.social
+* @see				https://github.com/Barkoczy/universal-lite
+* @license    https://universal.barkoczy.social/license
+*/
 declare(strict_types=1);
 
 // @Autoload
